4a255332c0025f056905e47e19c865cf51a505fd
MochIsrin068 <isrin068@gmail.com>
2023-06-28 23:02:39 +0800

    config

--- next.config.js
+++ next.config.js
@@ -5,18 +5,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.waresix.com",
-        port: "",
-        pathname: "/wp-content/**",
-      },
-      {
-        protocol: "https",
-        hostname: "iyansr.id",
-        port: "",
-        pathname: "/_next/**",
-      },
-      {
-        protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
         pathname: "/dj0mbpsar/image/upload/**",
