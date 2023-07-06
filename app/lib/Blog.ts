84eacc49b0ccaca3f7683e55c761f6ecd081f52d
MochIsrin068 <isrin068@gmail.com>
2023-07-06 22:12:22 +0800

    blog: disabled api caching

--- app/lib/Blog.ts
+++ app/lib/Blog.ts
@@ -2,7 +2,12 @@ export async function getDataBlog() {
   const res = await fetch(
     `https://api.rss2json.com/v1/api.json?${new URLSearchParams({
       rss_url: "https://medium.com/feed/@isrin068",
-    })}`
+    })}`,
+    {
+      next: {
+        revalidate: 0,
+      },
+    }
   );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
